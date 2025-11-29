27dca28939bc01ea952da52e97ba4ec5b5a89664
mayazar <57678669+mayazar@users.noreply.github.com>
2025-11-29 20:34:04 +0200

    Add files via upload

--- alef-bold.js
+++ alef-bold.js
@@ -1,18 +1,7 @@
 // Alef Bold font for jsPDF (Base64)
-(function (root) {
-
-  var jsPDFAPI =
-    root && root.jspdf && root.jspdf.jsPDF
-      ? root.jspdf.jsPDF.API
-      : (root && root.jsPDF ? root.jsPDF.API : null);
-
-  if (!jsPDFAPI) {
-    console.error("jsPDF not loaded – Alef Bold font not registered");
-    return;
-  }
 
   // *** כאן שימי את המחרוזת הארוכה של Alef Bold ***
-  var font = 'AAEAAAASAQAABAAgR0RFRtqTyEIAAA...הפונט_שלך...';
+  var AlefBold = 'AAEAAAASAQAABAAgR0RFRtqTyEIAAA...הפונט_שלך...';
 
   jsPDFAPI.addFileToVFS('Alef-Bold.ttf', font);
   jsPDFAPI.addFont('Alef-Bold.ttf', 'Alef', 'bold');
--- alef-regular.js
+++ alef-regular.js
@@ -1,19 +1,8 @@
 // Alef Regular font for jsPDF (Base64)
-(function (root) {
 
-  // מוצאים את API של jsPDF בצורה בטוחה
-  var jsPDFAPI =
-    root && root.jspdf && root.jspdf.jsPDF
-      ? root.jspdf.jsPDF.API
-      : (root && root.jsPDF ? root.jsPDF.API : null);
-
-  if (!jsPDFAPI) {
-    console.error("jsPDF not loaded – Alef Regular font not registered");
-    return;
-  }
 
   // *** כאן שימי את המחרוזת הארוכה של הפונט ***
-  var font = 'AAEAAAASAQAABAAgR0RFRrRCsIIAAAC8AAA...הפונט_שלך...';
+  var AlefRegular = 'AAEAAAASAQAABAAgR0RFRrRCsIIAAAC8AAA...הפונט_שלך...';
 
   jsPDFAPI.addFileToVFS('Alef-Regular.ttf', font);
   jsPDFAPI.addFont('Alef-Regular.ttf', 'Alef', 'normal');
